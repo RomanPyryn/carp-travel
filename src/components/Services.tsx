1745bee4b07169d3cf25a28795a2a6ca8cf094cc
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-08-22 19:25:11 +0300

    add swiper

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,29 +1,10 @@
+'use client';
+import Slider from './Slider';
+
 function Services() {
   return (
     <section className="services">
-      <div className="container">
-        <h2 className="section-title">WE OFFER</h2>
-        <div className="">
-          <span className=""></span>
-          <span className=""></span>
-        </div>
-        <picture></picture>
-        <p className="">Feel the adrenaline rush</p>
-
-        <ul className="">
-          <li className="bold">ATVs Traveling</li>
-          <li className="">Rock climbing</li>
-          <li className="">Hot air ballooning</li>
-          <li className="">Hot air ballooning</li>
-          <li className="">Rafting</li>
-        </ul>
-
-        <p className="">
-          Join exciting rafting expeditions on the waterways of the Carpathians. Go through
-          challenging waterways and overcome gusty waves, feel the adrenaline, and enjoy the
-          incredible views of the surrounding mountains.
-        </p>
-      </div>
+			<Slider/>
     </section>
   );
 }
