e98da3acc76e8bd497dd5f13460907b5d09f997a
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-08-23 15:41:36 +0300

    add swiper ts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,37 +2,37 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import logo from '../../public/images/header/logo.svg'
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-
-  
-  
-  const handleOpen = () => {
+	const [open, setOpen] = useState<boolean>(false);
+	
+	const handleOpen = () => {
 		setOpen(!open);
 		document.body.style.overflow = !open ? 'hidden' : 'visible';
-  };
-  return (
-    <header className="header absolute z-[3] w-full py-9 tracking-[0.0875rem]">
-      <div className="container">
-        <div className="flex justify-between">
-          <Link href="./">
-            <Image src="/images/header/logo.svg" width={61} height={34} alt="Logo" />
-          </Link>
-          <div className={open ? 'menu is-open backdrop-blur-xl' : 'menu'}>
-            <nav className="nav">
-              <Link href="#to-about">About</Link>
-              <Link href="#to-services">Services</Link>
-              <Link href="#to-career">Career</Link>
-              <Link href="#to-gallery">Gallery</Link>
-              <Link href="#to-contact">Contacts</Link>
-            </nav>
-          </div>
-          <button className="relative z-[5] uppercase md:hidden" onClick={handleOpen} type="button">
-            {!open ? 'Menu' : 'Close'}
-          </button>
-        </div>
-      </div>
-    </header>
-  );
+	};
+  
+	return (
+		<header className="header absolute z-[3] w-full py-9 tracking-[0.0875rem]">
+			<div className="container">
+				<div className="flex justify-between">
+					<Link href="./">
+						<Image src={logo} width={61} height={35} alt="Logo" />
+					</Link>
+					<div className={open ? 'menu is-open backdrop-blur-xl' : 'menu'}>
+						<nav className="nav">
+							<Link href="#to-about">About</Link>
+							<Link href="#to-services">Services</Link>
+							<Link href="#to-career">Career</Link>
+							<Link href="#to-gallery">Gallery</Link>
+							<Link href="#to-contact">Contacts</Link>
+						</nav>
+					</div>
+					<button className="relative z-[5] uppercase md:hidden" onClick={handleOpen} type="button">
+						{!open ? 'Menu' : 'Close'}
+					</button>
+				</div>
+			</div>
+		</header>
+	);
 }
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,6 +1,16 @@
 import { FC } from 'react';
+import Image, { StaticImageData } from 'next/image';
 
-const Slide = ({ slideNum, label, text, goTo1, goTo2, goTo3, goTo4, goTo5, index }) => {
+type slideProps = {
+  slideNum: string,
+  label: string,
+  text: string,
+  goTo: (index: number) => void,
+	index: number,
+	img: StaticImageData,
+};
+
+const Slide: FC<slideProps> = ({ slideNum, label, text, goTo, index, img }) => {
   return (
     <div className={`section slide0${slideNum} py-14 md:py-16 xl:py-[104px]`}>
       <div className="container smOnly:min-h-[740px]">
@@ -14,17 +24,16 @@ const Slide = ({ slideNum, label, text, goTo1, goTo2, goTo3, goTo4, goTo5, index
           </div>
         </div>
         <div className="relative smOnly:space-y-3 md:flex md:gap-5">
-          <picture>
-            <img
-              className="w-full md:w-[463px] md:h-[370px] xl:w-[607px] xl:h-[429px]"
-              width="280"
-              height="213"
-              loading="lazy"
-              alt="Nature"
-              src={`/public/images/services/serv-slide-${slideNum}.png`}
-              srcSet={`/images/services/serv-slide-${slideNum}@2x.png 2x`}
-            />
-          </picture>
+          <Image
+            className="w-full md:w-[463px] md:h-[370px] xl:w-[607px] xl:h-[429px]"
+            width={280}
+            height={213}
+            loading="lazy"
+            // placeholder="blur"
+            alt="Nature"
+            src={img}
+            // srcSet={`/images/services/serv-slide-${slideNum}@2x.png 2x`}
+          />
           <div className="flex flex-col justify-between gap-9 mdOnly:w-[221px]">
             <div className="smOnly:space-y-6 md:flex md:flex-col-reverse gap-6 xl:flex-row-reverse xl:gap-[70px]">
               <p className="font-extralight smOnly:text-end leading-[2] tracking-[2.4px] text-xs">
@@ -32,42 +41,42 @@ const Slide = ({ slideNum, label, text, goTo1, goTo2, goTo3, goTo4, goTo5, index
               </p>
               <ul className="text-white/20 uppercase text-[20px] leading-[0.85] space-y-4 smOnly:max-w-[190px] md:text-[18px] md:leading-[0.82] xl:text-[28px] xl:leading-[0.86] xl:w-[280px] xl:space-y-6">
                 <li
-                  className={index === 0 ? "current-item" : ""}
+                  className={index === 0 ? 'current-item' : ''}
                   aria-label="Go to slide 1"
                   role="button"
-                  onClick={goTo1}
+                  onClick={() => goTo(0)}
                 >
                   ATVs Traveling
                 </li>
                 <li
-                  className={index == 1 ? "current-item" : ""}
+                  className={index == 1 ? 'current-item' : ''}
                   aria-label="Go to slide 2"
                   role="button"
-                  onClick={goTo2}
+                  onClick={() => goTo(1)}
                 >
                   Rock climbing
                 </li>
                 <li
-                  className={index == 2 ? "current-item" : ""}
+                  className={index == 2 ? 'current-item' : ''}
                   aria-label="Go to slide 3"
                   role="button"
-                  onClick={goTo3}
+                  onClick={() => goTo(2)}
                 >
                   Hot air ballooning
                 </li>
                 <li
-                  className={index == 3 ? "current-item" : ""}
+                  className={index == 3 ? 'current-item' : ''}
                   aria-label="Go to slide 4"
                   role="button"
-                  onClick={goTo4}
+                  onClick={() => goTo(3)}
                 >
                   Skydiving
                 </li>
                 <li
-                  className={index == 4 ? "current-item" : ""}
+                  className={index == 4 ? 'current-item' : ''}
                   aria-label="Go to slide 5"
                   role="button"
-                  onClick={goTo5}
+                  onClick={() => goTo(4)}
                 >
                   Rafting
                 </li>
@@ -82,12 +91,5 @@ const Slide = ({ slideNum, label, text, goTo1, goTo2, goTo3, goTo4, goTo5, index
     </div>
   );
 };
-
-// type slideInfoProps = {
-//   slideNum: string;
-//   label: string;
-// 	text: string;
-// 	goTo: number;
-// };
 
 export default Slide;
