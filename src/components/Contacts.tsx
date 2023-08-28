71d5669fcbf5728f9cc45efcb1795ef8ec4c23cc
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-08-28 16:03:37 +0300

    add contacts markup and styles

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,7 +1,38 @@
-
-
 export default function Contacts() {
   return (
-    <></>
-  )
+    <section className="contacts section">
+      <div className="container">
+        <h2 className="section-title mb-9 xl:mb-20">
+          <span className="font-thin">Contact</span> us
+        </h2>
+        <div className="smOnly:space-y-6 mdOnly:grid mdOnly:grid-cols-2 mdOnly:mb-16 xl:flex xl:flex-col justify-between">
+          <div className="space-y-6 xl:space-y-16  xl:mb-32">
+            <div className="contacts-block justify-end">
+              <div className="">
+                <a href="tel:+380981234567" className="contacts-link">+38 (098) 12 34 567</a>
+								<a href="tel:+380731234567" className="contacts-link">+38 (073) 12 34 567</a>
+              </div>
+              <p className="contacts-label">Phone number</p>
+            </div>
+            <div className="contacts-block justify-end">
+              <div className="">
+                <a href="mailto:support@carptravel.com" className="contacts-link">support@carptravel.com</a>
+              </div>
+              <p className="contacts-label">E-mail</p>
+            </div>
+          </div>
+					<div className="contacts-block flex-row-reverse justify-start  md:justify-end xl:flex-row xl:self-end">
+              <div className="w-[100px]">
+                <a href={'facebook.com'} className="contacts-link">facebook</a>
+                <a href={'https://www.instagram.com/'} className="contacts-link">instagram</a>
+                <a href={'https://www.youtube.com'} className="contacts-link">youtube</a>
+                <a href={'https://www.tiktok.com/'} className="contacts-link">tiktok</a>
+              </div>
+              <p className="contacts-label text-end xl:text-start">Follow us</p>
+            
+					</div>
+        </div>
+      </div>
+    </section>
+  );
 }
